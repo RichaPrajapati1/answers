4f3c70827f103e7820a53c1072900114d3f43ef9
danielsgriffin <danielsgriffin@gmail.com>
2015-08-17 01:47:12 -0700

    all prior HW

---
+++ HW06/HW06_ex09_03.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# HW06_ex09_03.py
+
+# (1)
+# Write a function named avoids that takes a word and a string of forbidden
+# letters, and that returns True if the word doesn't use any of the forbidden
+# letters.
+#   - write avoids
+# (2)
+# Modify your program to prompt the user to enter a string of forbidden
+# letters and then print the number of words that don't contain any of them.
+#   - write function(s)
+# (3)
+# Can you find a combination of 5 forbidden letters that excludes the smallest
+# number of words?
+#   - write a function that finds this combination of letters
+#   - have that function print the letters and print the # of words excluded
+##############################################################################
+# Imports
+import string
+import itertools
+
+# Body
+
+
+def avoids(word, forbidden_string):
+    """ return True if word NOT forbidden"""
+    for char in forbidden_string:
+        if char in word:
+            return False
+    return True
+
+
+def forbidden_prompt():
+    """ print count of words NOT forbidden by input"""
+    forbidden_string = raw_input(">")
+    count_clean = 0
+    with open("words.txt", "r") as f:
+        for word in f:
+            if avoids(word, forbidden_string):
+                count_clean += 1
+    print count_clean
+
+def forbidden_param(words, forbidden_string):
+    """ return count of words NOT forbidden by param"""
+    count_clean = 0
+    for word in words:
+        if avoids(word, forbidden_string):
+            count_clean += 1
+    return count_clean
+
+
+def find_five():
+    count_clean = 0
+    excludes_smallest = ''
+    combos = list(itertools.combinations(string.ascii_lowercase, 5))
+    with open("words.txt", "r") as f:
+        words = f.readlines()
+        for each in combos:
+            tmp_clean = forbidden_param(words, each)
+            if tmp_clean > count_clean:
+                count_clean = tmp_clean
+                excludes_smallest = each
+    print count_clean, excludes_smallest
+
+
+##############################################################################
+def main():
+    # forbidden_prompt()
+    # find_five() # 96425 ('j', 'q', 'w', 'x', 'z')
+
+if __name__ == '__main__':
+    main()
